c9e8934ac70a3b0e2933fcf84242241c19f69bf7
charistakeiko <charistakeiko@yahoo.com>
2016-05-04 02:02:07 +0700

    half todo

--- DailyDiary/App.xaml.cs
+++ DailyDiary/App.xaml.cs
@@ -14,37 +14,32 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using MySql.Data.MySqlClient;
+using DailyDiary.ViewModel;
 
 namespace DailyDiary
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
+
     sealed partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
+        public static TodoViewModel TODO_VIEW_MODEL = new TodoViewModel();
         public App()
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
+
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
+
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
+
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
@@ -65,18 +60,15 @@ namespace DailyDiary
                 Window.Current.Content = rootFrame;
             }
 
-            if (e.PrelaunchActivated == false)
+            if (rootFrame.Content == null)
             {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
             }
+            // Ensure the current window is active
+            Window.Current.Activate();
         }
 
         /// <summary>
--- DailyDiary/MainPage.xaml.cs
+++ DailyDiary/MainPage.xaml.cs
@@ -12,6 +12,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using MySql;
+using DailyDiary.ViewModel;///nama class
+using System.Collections.ObjectModel;
+using DailyDiary.Model;///nama class
+/// </summary>
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +27,21 @@ namespace DailyDiary
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void InsertTodoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Try the the View Model insertion and check externally for result
+            App.TODO_VIEW_MODEL.InsertNewTodo(NewTodoTxtBox.Text);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Todos.ItemsSource = App.TODO_VIEW_MODEL.GetTodos();
         }
     }
 }
